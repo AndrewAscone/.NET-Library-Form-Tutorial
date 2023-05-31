e01fd183d4ef68d3c96ce506aefebe0519955966
AndrewAscone <andrew.ascone@gmail.com>
2023-05-31 19:31:20 -0400

    Small adjustment to main form

--- BookApp/MainForm.Designer.cs
+++ BookApp/MainForm.Designer.cs
@@ -78,7 +78,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(787, 424);
             Controls.Add(btnBookDelete);
             Controls.Add(btnOpenEditForm);
             Controls.Add(btnNewForm);
